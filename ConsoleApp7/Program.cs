aae8ce167d747e5b5d319c49468e76f4b493c8e8
Gaston <cparwez@gmail.com>
2018-09-19 14:51:36 +0100

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+        }
+    }
+}
